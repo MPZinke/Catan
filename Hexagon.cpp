53e328592d167d8bc95696f90c71d639170b4032
mpzinke <mathew.zinke@gmail.com>
2022-12-05 20:14:37 -0600

    Starts work on hexagons for board

---
+++ Hexagon.cpp
@@ -0,0 +1,65 @@
+
+
+/*
+       ONE    SIX
+      \ | /
+        ⬢
+
+  THREE    FOUR
+*/
+enum CornerType
+{
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX
+}
+
+
+/*
+    ONE    SIX
+       \ /
+  TWO — ⬢ — FIVE
+       / \
+  THREE    FOUR
+*/
+enum SideType
+{
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX
+}
+
+
+
+enum HexagonType
+{
+	DeserT,
+	WOOD,
+	STONE,
+	BRICK,
+	WHEAT,
+	SHEEP
+};
+
+
+class Side
+{
+	private:
+		int
+}
+
+
+class Hexagon
+{
+	private:
+		int _id;
+		HexagonType _type;
+
+}
+
